1aeea2dfa1fcf717f9d10cf0b893fa06737d6780
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-19 21:55:12 +0300

    Proje dosyası ekle.

---
+++ DecimalVariables2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalVariables2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //decimal sayi1,sayi2,fark;
+            //sayi1 = 33;
+            //sayi2 = 10;
+            //fark = sayi1 - sayi2;
+            //Console.WriteLine(fark);
+
+
+            //Karenin alan ve cevresini decimal türünde hesaplayalım;
+
+            //decimal kenar, cevre, alan;
+            //Console.WriteLine("Lütfen kenar uzunluğu giriniz");
+            //kenar = Convert.ToDecimal(Console.ReadLine());
+            //cevre = kenar * 4;
+            //alan = kenar * kenar;
+            //Console.WriteLine("Cevre : " + cevre + " " + "Alan : " + alan);
+
+            //decimal türüne kod kısmından ondalıklı deger nasıl atanır;
+            decimal sayi;
+            sayi = 8.9m;
+            Console.WriteLine(sayi);
+
+            Console.Read();
+        }
+    }
+}
